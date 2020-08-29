b24ad79204f2567f95b168bf11d4c8c24541436f
Sayan Biswas <66244072+sayanbiswas-india@users.noreply.github.com>
2020-08-29 12:50:27 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,86 +1,142 @@
+#importing required modules
 from random import randrange
 import pygame
 from pygame import Color, Surface, event, image, key
-import random
-import math
+#initializing pygame
 pygame.init()
-icon = pygame.image.load(r"D:\\icon.png")
-blue=(0,0,255)
-red =(255,0,0)
-yellow = (255,255,0)
-forest_green = (34,139,34)
-screen_size = (720,600)
+#initializing variables
+blue = (0, 0, 255)
+
+red = (255, 0, 0)
+
+yellow = (255, 255, 0)
+
+forest_green = (34, 139, 34)
+
+screen_size = (720, 600)
+
 title = 'Snake Game'
+
 vel = 0
+
 vel_1 = 0
-dim = (30,30)
-x = random.randrange(100,690)
-y = randrange(50,550)
-x_enemy = random.randrange(100,690)
-y_enemy = randrange(50,550)
+
+dim = (30, 30)
+
+x = random.randrange(100, 690)
+
+y = randrange(50, 550)
+
+x_enemy = random.randrange(100, 690)
+
+y_enemy = randrange(50, 550)
+#setting game window where game will be played
 screen = pygame.display.set_mode(screen_size)
+#setting game name
 caption = pygame.display.set_caption(title)
 
-def snake(surface,color):
-    for p , z in snake_list:
-       pygame.draw.rect(surface,color,[p,z,30,30])
-def food(surface,color,centre,radius):
-    pygame.draw.circle(surface,color,centre,radius)
+#function for blitting rectangle(snake)
+def snake(surface, color):
+    for p, z in snake_list:
+        pygame.draw.rect(surface, color, [p, z, 30, 30])
+
+#function for blitting circle(food)
+def food(surface, color, centre, radius):
+    pygame.draw.circle(surface, color, centre, radius)
+
+#displaying score
 def score_display():
     font = pygame.font.SysFont(None, 40)
-    img = font.render("Score :" + str(score*10), True, forest_green )
+    img = font.render("Score :" + str(score*10), True, forest_green)
     screen.blit(img, (10, 10))
 
-score = 0              
+#starting score
+score = 0
+#game state (True or false)
 running = True
-snake_head = 1 
-snake_list = [] 
+#default snake length
+snake_head = 1
+#list for appending snake heads as snake eats food
+snake_list = []
+#while loop for continuously until stopped
 while running:
-    centre = (x_enemy,y_enemy)  
+    #food position
+    centre = (x_enemy, y_enemy)
+    #initializing keyboard keys
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False   
+            running = False
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             vel = -0.7
             vel_1 = 0
+
         if keys[pygame.K_RIGHT]:
             vel = 0.7
             vel_1 = 0
+
         if keys[pygame.K_UP]:
-           vel_1 = -0.7
-           vel = 0
+            vel_1 = -0.7
+            vel = 0
+
         if keys[pygame.K_DOWN]:
-           vel_1 = 0.7
-           vel = 0
+            vel_1 = 0.7
+            vel = 0
+
         if keys[pygame.K_ESCAPE]:
             running = False
-    if x >= 690 :
+
+    if x >= 690:
         running = False
-    if x <= 0 :
+
+    if x <= 0:
         running = False
+
     if y >= 570:
         running = False
-    if y <= 0 :
+
+    if y <= 0:
         running = False
+    #used for making the snake move continuously
     x += vel
-    y += vel_1 
-    if abs(x - x_enemy)< 18 and abs(y - y_enemy)< 18:
-        score +=1
-        snake_head +=20
+
+    y += vel_1
+    #if statement for collision when the position of snake and food equals or is nearby(set accordingly)
+    if abs(x - x_enemy) < 18 and abs(y - y_enemy) < 18:
+        #increment score when snake eats food
+        score += 1
+        #increase length of snake when snake eats food
+        snake_head += 20
+        #changing position of food when snake eats food
         x_enemy = random.randint(20, 690)
+
         y_enemy = random.randint(20, 550)
+
+    #code for increasing snake length
     head = []
+
     head.append(x)
+
     head.append(y)
-    snake_list.append(head)     
-       
+
+    snake_list.append(head)
+
+    #code for cutting heads
     if len(snake_list) > snake_head:
-        del snake_list[0]   
+   
+        del snake_list[0]
+    #end game if snake eats itself
     if head in snake_list[:-1]:
-        running = False                   
-    screen.fill((255, 255, 255))              
-    snake(screen,blue)
-    food(screen,red,centre,7)
+    
+        running = False
+    #filling colors on screen
+    screen.fill((255, 255, 255))
+    #calling functions
+    snake(screen, blue)
+
+    food(screen, red, centre, 7)
+
     score_display()
-    pygame.display.update() 
+    #function for updating contents on screen
+    pygame.display.update()
